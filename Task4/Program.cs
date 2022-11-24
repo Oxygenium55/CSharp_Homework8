d7864c2e0e733849c8f1a32c5569ccb8a0f7c5c8
Oxygen <grandnorik@mail.ru>
2022-11-24 13:56:12 +0600

    Task4 done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -7,4 +7,50 @@
 26(1,0,1) 55(1,1,1)
 */
 
+int GetNumber(string message)
+{
+    Console.Write(message);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
 
+int[,,] InitArray(int x, int y, int z)
+{
+    int[,,] array = new int[x, y, z];
+    Random rnd = new Random();
+    int temp = rnd.Next(10, 20);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = temp;
+                temp = temp + rnd.Next(1,10);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k})  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int x = GetNumber("Задайте размер x: ");
+int y = GetNumber("Задайте размер y: ");
+int z = GetNumber("Задайте размер z: ");
+Console.WriteLine();
+int[,,] array = InitArray(x, y, z);
+PrintArray(array);
